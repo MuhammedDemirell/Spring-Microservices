2fcb647cd929e2d0de3bf61a7f5470cb51d3f823
Muhammed Demirel <muhammed_demirell@hotmail.com>
2023-08-08 14:18:36 +0300

    update .yaml file and pım.xml

--- product-service/product-service/src/main/java/com/demirel/stockmanagement/productservice/ProductServiceApplication.java
+++ product-service/product-service/src/main/java/com/demirel/stockmanagement/productservice/ProductServiceApplication.java
@@ -3,10 +3,12 @@ package com.demirel.stockmanagement.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableEurekaClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
