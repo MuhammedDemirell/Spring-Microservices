3f3a5a90aca99810e348e12535f5266465bac98b
Muhammed Demirel <muhammed_demirell@hotmail.com>
2023-08-10 02:38:50 +0300

    updat zuul edge yaml,pom.xml and annotation

--- zuul-edge-server/zuul-edge-server/src/main/java/com/eleiatech/stockmagement/zuuledgeserver/ZuulEdgeServerApplication.java
+++ zuul-edge-server/zuul-edge-server/src/main/java/com/eleiatech/stockmagement/zuuledgeserver/ZuulEdgeServerApplication.java
@@ -3,15 +3,13 @@ package com.eleiatech.stockmagement.zuuledgeserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @EnableDiscoveryClient
-@EnableEurekaClient
 @SpringBootApplication
-@EnableZuulProxy
+
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class ZuulEdgeServerApplication {
 
